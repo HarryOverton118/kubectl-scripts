6eec0145abd3fd35f691acecdb286f6ea50ebc56
HarryOverton118 <74662400+HarryOverton118@users.noreply.github.com>
2021-07-25 21:30:30 +0100

    Create kube-alias.sh
    simple alias

---
+++ kube-alias.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# kubectl commands
+alias k='kubectl'
+# kubectl = k
+
+alias kc='kubectl create'
+# kubectl create = kc
+
+alias kf='kubectl create -f'
+# kubectl create -f = kf
+
+alias kaf='kubectl apply -f'
+# kubectl apply -f = kaf
+
+alias kd='kubectl delete'
+# kubectl delete = kd
+
+
+#  commands for pods
+alias kgp='kubectl get pods'
+# kubectl get pods = kgp
+
+alias kdp='kubectl describe pods'
+# kubectl describe pods = kdp
+
+alias kr='kubectl run'
+# kubectl run = kr
+
+
+# commsndds for  deployments
+alias kgd='kubectl get deployments'
+# kubectl get deployments = kgd
+
+alias kdd='kubectl describe deployments'
+# kubectl describe deployments = kdd
+
+# commands for  namespaces
+alias kgn='kubectl get namespaces'
+# kubectl get namespaces = kgn
+
+alias kdn='kubectl describe namespaces'
+# kubectl describe namespaces = kdn
+
+alias kcn='kubectl create namespace'
+# kubectl create namespace = kcn
